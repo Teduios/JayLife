d1d781d16e6e7092598f71c9c40b2f732d73db2e
沈徐洁 <sxj9113q@sina.cn>
2016-04-27 22:12:03 +0800

    Initial Commit

---
+++ 生活大爆炸/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  生活大爆炸
+//
+//  Created by 沈徐洁 on 16/4/27.
+//  Copyright © 2016年 沈徐洁. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 生活大爆炸/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  生活大爆炸
+//
+//  Created by 沈徐洁 on 16/4/27.
+//  Copyright © 2016年 沈徐洁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
